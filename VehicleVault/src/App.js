3811801c5286df2138eb4b3650475452912da327
benny4man <benny4man@gmail.com>
2025-01-29 18:58:46 -0500

    .

---
+++ VehicleVault/src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/Welcome";
+import LogIn from "./screens/LogIn";
+import MaintenanceScreen from "./screens/Maintenance";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+    
+    // NOTE FOR TESTING, when testing your screen create an instance for it under 
+    //  Stack.Navigator like what is below. Comment or uncomment as needed, what 
+    //  is uncommented on top of the list will be the starting screen
+
+    return (
+        <SafeAreaProvider>
+            <StatusBar backgroundColor="#D9D9D9"/>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Maintenance"
+                        component={MaintenanceScreen}
+                        options={{ headerShown: false }}
+                    />
+                    {/* Home Screen */}
+                    {/* <Stack.Screen
+						name="Home"
+						component={HomeScreen}
+						options={{ headerShown: false }}
+					/>
+
+					
+					<Stack.Screen name="LogIn" component={LogIn} /> */}
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
+}
