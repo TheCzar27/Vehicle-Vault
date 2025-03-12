3c9e199e8207eeacee89b6473d6f5e0edb9ff126
benny4man <benny4man@gmail.com>
2025-03-12 14:17:37 -0400

    modified nav to use both tab and stack

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WelcomeScreen from "./src/screens/Welcome";
 import LogInScreen from "./src/screens/LogIn";
 import RegisterScreen from "./src/screens/Register";
@@ -14,33 +15,51 @@ import Analytics from "./src/screens/Analytics";
 import Settings from "./src/screens/Settings";
 import { StatusBar } from "expo-status-bar";
 
-const Stack = createStackNavigator();
+const RootStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const FinancesStack = createStackNavigator();
+
+// stack nav for finanances and its subscreens
+function FinancesScreens() {
+	return (
+		<FinancesStack.Navigator screenOptions={{ headerShown: false }}>
+			<FinancesStack.Navigator name="FinanceMain" component={Finances} />
+			<FinancesStack.Navigator name="Analytics" component={Analytics} />
+			<FinancesStack.Navigator name="AddPayment" component={AddPayment} />
+		</FinancesStack.Navigator>
+	);
+}
+
+// tab nav for main screens
+function MainScreensNav() {
+	return (
+		<Tab.Navigator>
+			<Tab.Navigator name="Garage" component={GarageScreen} />
+			<Tab.Navigator name="Maintenance" component={MaintenanceScreen} />
+			<Tab.Navigator name="Finances" component={FinancesScreens} />
+			<Tab.Navigator name="Map" component={Map} />
+			<Tab.Navigator name="Settings" component={Settings} />
+		</Tab.Navigator>
+	);
+}
 
 export default function App() {
-    // NOTE FOR TESTING, when testing your screen create an instance for it under
-    //  Stack.Navigator like what is below. Comment or uncomment as needed, what
-    //  is uncommented on top of the list will be the starting screen
+	// NOTE FOR TESTING, when testing your screen create an instance for it under
+	//  Stack.Navigator like what is below. Comment or uncomment as needed, what
+	//  is uncommented on top of the list will be the starting screen
 
-    return (
-        <SafeAreaProvider>
-            <StatusBar backgroundColor="#D9D9D9" />
-            <NavigationContainer>
-                <Stack.Navigator screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="Welcome" component={WelcomeScreen} />
-                    <Stack.Screen name="LogIn" component={LogInScreen} />
-                    <Stack.Screen name="Register" component={RegisterScreen} />
-                    <Stack.Screen
-                        name="Maintenance"
-                        component={MaintenanceScreen}
-                    />
-                    <Stack.Screen name="Garage" component={GarageScreen} />
-                    <Stack.Screen name="Map" component={Map} />
-                    <Stack.Screen name="Settings" component={Settings} />
-                    <Stack.Screen name="Finances" component={Finances} />
-                    <Stack.Screen name="AddPayment" component={AddPayment} />
-                    <Stack.Screen name="Analytics" component={Analytics} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaProvider>
-    );
+	return (
+		<SafeAreaProvider>
+			<StatusBar backgroundColor="#D9D9D9" />
+			<NavigationContainer>
+				<RootStack.Navigator screenOptions={{ headerShown: false }}>
+					<RootStack.Screen name="Welcome" component={WelcomeScreen} />
+					<RootStack.Screen name="LogIn" component={LogInScreen} />
+					<RootStack.Screen name="Register" component={RegisterScreen} />
+					{/* below are main screens */}
+					<RootStack.Screen name="Main" component={MainScreensNav} />
+				</RootStack.Navigator>
+			</NavigationContainer>
+		</SafeAreaProvider>
+	);
 }
